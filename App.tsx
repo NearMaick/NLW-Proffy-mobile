ed4582535f4c8908de456633dc5581345d098081
Maick Souza <maick_a_s@msn.com>
2020-08-07 13:00:00 -0300

    chore(mobile): initial setting

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Maick Souza</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f00',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+// parei aqui 20'05
