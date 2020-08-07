6751248b304b465b14e31cc107d58c767beab49f
Maick Souza <maick_a_s@msn.com>
2020-08-07 13:16:30 -0300

    chore(mobile): initial setting

--- App.tsx
+++ App.tsx
@@ -1,23 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Landing from './src/pages/Landing';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Maick Souza</Text>
+    <>   
+      <Landing />
       <StatusBar style="auto" />
-    </View>
+    </>   
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f00',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-// parei aqui 20'05
