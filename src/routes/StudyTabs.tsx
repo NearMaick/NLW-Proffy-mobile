6490abce831e46679a264261a223bb1b842f2e3a
Maick Souza <maick_a_s@msn.com>
2020-08-07 16:22:00 -0300

    style(mobile): TabBar #1

--- src/routes/StudyTabs.tsx
+++ src/routes/StudyTabs.tsx
@@ -1,5 +1,6 @@
  import React from 'react';
  import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+ import { Ionicons } from '@expo/vector-icons';
 
 import TeacherList from '../pages/TeacherList';
 import Favorites from '../pages/Favorites';
@@ -8,8 +9,46 @@ import Favorites from '../pages/Favorites';
 
  function StudyTabs() {
    return (
-     <Navigator>
-       <Screen name="TeacherList" component={TeacherList} />
+     <Navigator
+      tabBarOptions={{
+        style: {
+          elevation: 0,
+          shadowOpacity: 0,
+          height: 64,
+        },
+        tabStyle: {
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+        iconStyle: {
+          flex: 0,
+          width: 20,
+          height: 20,
+        },
+        labelStyle: {
+          fontFamily: 'Archivo_700Bold',
+          fontSize: 13,
+          marginLeft: 16,
+        },
+        inactiveBackgroundColor: '#fafafc',
+        activeBackgroundColor: '#ebebf5',
+        inactiveTintColor: '#c1bccc',
+        activeTintColor: '#32264d',
+      }}
+     >
+       <Screen 
+        name="TeacherList" 
+        component={TeacherList} 
+        options={{
+          tabBarLabel: 'Proffys',
+          tabBarIcon: () => {
+            return (
+              <Ionicons name="ios-easel" />
+            );
+          }
+        }}
+       />
        <Screen name="Favorites" component={Favorites} />
      </Navigator>
    )
